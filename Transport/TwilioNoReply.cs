4437e6d04df4365ccf975f0fbfe7458e3d6d242e
Arran <arranf@gmail.com>
2015-07-31 22:58:36 +0100

    Initial release

---
+++ Transport/TwilioNoReply.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+
+using Twilio;
+
+namespace Rock.Communication.Transport
+{
+    /// <summary>
+    /// Communication transport for sending no-reply SMS messages using Twilio
+    /// </summary>
+    [Description( "Sends a no-reply communication through Twilio API" )]
+    [Export( typeof( TransportComponent ) )]
+    [ExportMetadata( "ComponentName", "No-Reply Twilio" )]
+    [TextField( "SID", "Your Twilio Account SID (find at https://www.twilio.com/user/account)", true, "", "", 0 )]
+    [TextField( "Token", "Your Twilio Account Token", true, "", "", 1 )]
+
+    public class TwilioNoReply : TransportComponent
+    {
+        /// <summary>
+        /// Sends the specified communication.
+        /// </summary>
+        /// <param name="communication">The communication.</param>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override void Send( Rock.Model.Communication communication )
+        {
+            using ( var rockContext = new RockContext() )
+            {
+                // Requery the Communication
+                communication = new CommunicationService( rockContext ).Get( communication.Id );
+
+                if ( communication != null &&
+                    communication.Status == Model.CommunicationStatus.Approved &&
+                    communication.Recipients.Where( r => r.Status == Model.CommunicationRecipientStatus.Pending ).Any() &&
+                    (!communication.FutureSendDateTime.HasValue || communication.FutureSendDateTime.Value.CompareTo( RockDateTime.Now ) <= 0) )
+                {
+                    // Remove all non alpha numeric from fromValue
+                    string fromValue = new string( communication.GetMediumDataValue( "FromValue" ).Where( c => char.IsLetterOrDigit( c ) || char.IsWhiteSpace( c ) ).ToArray() );
+
+                    if ( !string.IsNullOrWhiteSpace( fromValue ) )
+                    {
+                        string accountSid = GetAttributeValue( "SID" );
+                        string authToken = GetAttributeValue( "Token" );
+                        var twilio = new TwilioRestClient( accountSid, authToken );
+
+                        var historyService = new HistoryService( rockContext );
+                        var recipientService = new CommunicationRecipientService( rockContext );
+
+                        var personEntityTypeId = EntityTypeCache.Read( "Rock.Model.Person" ).Id;
+                        var communicationEntityTypeId = EntityTypeCache.Read( "Rock.Model.Communication" ).Id;
+                        var communicationCategoryId = CategoryCache.Read( Rock.SystemGuid.Category.HISTORY_PERSON_COMMUNICATIONS.AsGuid(), rockContext ).Id;
+
+                        var globalConfigValues = GlobalAttributesCache.GetMergeFields( null );
+
+                        bool recipientFound = true;
+                        while ( recipientFound )
+                        {
+                            var recipient = Rock.Model.Communication.GetNextPending( communication.Id, rockContext );
+                            if ( recipient != null )
+                            {
+                                try
+                                {
+                                    var phoneNumber = recipient.PersonAlias.Person.PhoneNumbers
+                                        .Where( p => p.IsMessagingEnabled )
+                                        .FirstOrDefault();
+
+                                    if ( phoneNumber != null )
+                                    {
+                                        // Create merge field dictionary
+                                        var mergeObjects = recipient.CommunicationMergeValues( globalConfigValues );
+                                        StringBuilder messageBuilder = new StringBuilder( communication.GetMediumDataValue( "Message" ) );
+                                        if ( !string.IsNullOrWhiteSpace( communication.GetMediumDataValue( "SenderPhone" ) ) )
+                                        {
+                                            messageBuilder.Append( string.Format( "\nThis message was sent by {0} on behalf of {1} from a no reply number. To reply to this message send your response to {2}.", communication.GetMediumDataValue( "SenderName" ), Rock.Web.Cache.GlobalAttributesCache.Read().GetValueFormatted( "OrganizationName" ), communication.GetMediumDataValue( "SenderPhone" ) ) );
+                                        }
+                                        else
+                                        {
+                                            messageBuilder.Append( string.Format( "\nThis message was sent by {0} on behalf of {1} from a no reply number. To reply to this message contact {0} directly.", communication.GetMediumDataValue( "SenderName" ), Rock.Web.Cache.GlobalAttributesCache.Read().GetValueFormatted( "OrganizationName" ) ) );
+                                        }
+                                        string message = messageBuilder.ToString();
+                                        message = message.ResolveMergeFields( mergeObjects );
+
+                                        string twilioNumber = phoneNumber.Number;
+                                        if ( !string.IsNullOrWhiteSpace( phoneNumber.CountryCode ) )
+                                        {
+                                            twilioNumber = "+" + phoneNumber.CountryCode + phoneNumber.Number;
+                                        }
+
+                                        var globalAttributes = Rock.Web.Cache.GlobalAttributesCache.Read();
+                                        string callbackUrl = globalAttributes.GetValue( "PublicApplicationRoot" ) + "Webhooks/Twilio.ashx";
+
+                                        var response = twilio.SendMessage( fromValue, twilioNumber, message, callbackUrl );
+
+                                        recipient.Status = CommunicationRecipientStatus.Delivered;
+                                        recipient.TransportEntityTypeName = this.GetType().FullName;
+                                        recipient.UniqueMessageId = response.Sid;
+
+                                        try
+                                        {
+                                            historyService.Add( new History
+                                            {
+                                                CreatedByPersonAliasId = communication.SenderPersonAliasId,
+                                                EntityTypeId = personEntityTypeId,
+                                                CategoryId = communicationCategoryId,
+                                                EntityId = recipient.PersonAlias.PersonId,
+                                                Summary = "Sent No Reply SMS message.",
+                                                Caption = message.Truncate( 200 ),
+                                                RelatedEntityTypeId = communicationEntityTypeId,
+                                                RelatedEntityId = communication.Id
+                                            } );
+                                        }
+                                        catch ( Exception ex )
+                                        {
+                                            ExceptionLogService.LogException( ex, null );
+                                        }
+
+                                    }
+                                    else
+                                    {
+                                        recipient.Status = CommunicationRecipientStatus.Failed;
+                                        recipient.StatusNote = "No Phone Number with Messaging Enabled";
+                                    }
+                                }
+                                catch ( Exception ex )
+                                {
+                                    recipient.Status = CommunicationRecipientStatus.Failed;
+                                    recipient.StatusNote = "Twilio Exception: " + ex.Message;
+                                }
+
+                                rockContext.SaveChanges();
+                            }
+                            else
+                            {
+                                recipientFound = false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sends the specified template.
+        /// </summary>
+        /// <param name="template">The template.</param>
+        /// <param name="recipients">The recipients.</param>
+        /// <param name="appRoot">The application root.</param>
+        /// <param name="themeRoot">The theme root.</param>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override void Send( SystemEmail template, List<RecipientData> recipients, string appRoot, string themeRoot )
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Sends the specified medium data to the specified list of recipients.
+        /// </summary>
+        /// <param name="mediumData">The medium data.</param>
+        /// <param name="recipients">The recipients.</param>
+        /// <param name="appRoot">The application root.</param>
+        /// <param name="themeRoot">The theme root.</param>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override void Send( Dictionary<string, string> mediumData, List<string> recipients, string appRoot, string themeRoot )
+        {
+            try
+            {
+                var globalAttributes = GlobalAttributesCache.Read();
+
+                string fromValue = string.Empty;
+                mediumData.TryGetValue( "FromValue", out fromValue );
+                if ( !string.IsNullOrWhiteSpace( fromValue ) )
+                {
+                    string accountSid = GetAttributeValue( "SID" );
+                    string authToken = GetAttributeValue( "Token" );
+                    var twilio = new TwilioRestClient( accountSid, authToken );
+
+                    string message = string.Empty;
+                    string senderPhone = string.Empty;
+                    string senderName = string.Empty;
+                    mediumData.TryGetValue( "Message", out message );
+                    mediumData.TryGetValue( "SenderPhone", out senderPhone );
+                    mediumData.TryGetValue( "SenderName", out senderName );
+                    StringBuilder messageBuilder = new StringBuilder( message );
+                    if ( !string.IsNullOrWhiteSpace( senderPhone ) )
+                    {
+                        messageBuilder.Append( string.Format( "\nThis message was sent by {0} on behalf of {1} from a no reply number. To reply to this message send your response to {2}.", senderName, Rock.Web.Cache.GlobalAttributesCache.Read().GetValueFormatted( "OrganizationName" ), senderPhone ) );
+                    }
+                    else
+                    {
+                        messageBuilder.Append( string.Format( "\nThis message was sent by {0} on behalf of {1} from a no reply number. To reply to this message contact {0} directly.", senderName, Rock.Web.Cache.GlobalAttributesCache.Read().GetValueFormatted( "OrganizationName" ) ) );
+                    }
+                    message = messageBuilder.ToString();
+
+                    if ( !string.IsNullOrWhiteSpace( themeRoot ) )
+                    {
+                        message = message.Replace( "~~/", themeRoot );
+                    }
+
+                    if ( !string.IsNullOrWhiteSpace( appRoot ) )
+                    {
+                        message = message.Replace( "~/", appRoot );
+                        message = message.Replace( @" src=""/", @" src=""" + appRoot );
+                        message = message.Replace( @" href=""/", @" href=""" + appRoot );
+                    }
+
+                    foreach ( var recipient in recipients )
+                    {
+                        var response = twilio.SendMessage( fromValue, recipient, message );
+                    }
+                }
+            }
+
+            catch ( Exception ex )
+            {
+                ExceptionLogService.LogException( ex, null );
+            }
+        }
+
+        /// <summary>
+        /// Sends the specified recipients.
+        /// </summary>
+        /// <param name="recipients">The recipients.</param>
+        /// <param name="from">From.</param>
+        /// <param name="subject">The subject.</param>
+        /// <param name="body">The body.</param>
+        /// <param name="appRoot">The application root.</param>
+        /// <param name="themeRoot">The theme root.</param>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override void Send( List<string> recipients, string from, string subject, string body, string appRoot = null, string themeRoot = null )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UI/NoReplySMS.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Rock.Field;
+using Rock.Model;
+using Rock.Web.Cache;
+
+namespace Rock.Web.UI.Controls.Communication
+{
+    /// <summary>
+    /// SMS Communication Medium control
+    /// </summary>
+    public class NoReplySMS : MediumControl
+    {
+        #region UI Controls
+
+        private RockTextBox tbFrom;
+        private RockLiteral lFrom;
+        private RockControlWrapper rcwMessage;
+        private MergeFieldPicker mfpMessage;
+        private RockTextBox tbMessage;
+        private HiddenField hfSenderPhone;
+        private HiddenField hfSenderName;
+
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets a value indicating whether [use simple mode].
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [use simple mode]; otherwise, <c>false</c>.
+        /// </value>
+        public bool UseSimpleMode
+        {
+            get { return ViewState["UseSimpleMode"] as Boolean? ?? false; }
+            set { ViewState["UseSimpleMode"] = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the medium data.
+        /// </summary>
+        /// <value>
+        /// The medium data.
+        /// </value>
+        public override Dictionary<string, string> MediumData
+        {
+            get
+            {
+                EnsureChildControls();
+                var data = new Dictionary<string, string>();
+                if ( !UseSimpleMode )
+                {
+                    data.Add( "FromValue", tbFrom.Text );
+                    data.Add( "Subject", tbFrom.Text != null ? ("From: " + tbFrom.Text) : string.Empty );
+                }
+                else
+                {
+                    data.Add( "FromValue", lFrom.Text );
+                    data.Add( "Subject", lFrom.Text != null ? ("From: " + lFrom.Text) : string.Empty );
+                }
+                data.Add( "Message", tbMessage.Text );
+                data.Add( "SenderName", hfSenderName.Value );
+                data.Add( "SenderPhone", hfSenderPhone.Value );
+                return data;
+            }
+
+            set
+            {
+                EnsureChildControls();
+                lFrom.Text = GetDataValue( value, "FromValue" );
+                tbFrom.Text = GetDataValue( value, "FromValue" );
+                tbMessage.Text = GetDataValue( value, "Message" );
+                hfSenderName.Value = GetDataValue( value, "SenderName" );
+                hfSenderPhone.Value = GetDataValue( value, "SenderPhone" );
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Email"/> class.
+        /// </summary>
+        public NoReplySMS( )
+            : base()
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Email"/> class.
+        /// </summary>
+        /// <param name="useSimpleMode">if set to <c>true</c> [use simple mode].</param>
+        public NoReplySMS( bool useSimpleMode )
+            : this()
+        {
+            UseSimpleMode = useSimpleMode;
+        }
+        #endregion
+
+        #region CompositeControl Methods
+
+        /// <summary>
+        /// Called by the ASP.NET page framework to notify server controls that use composition-based implementation to create any child controls they contain in preparation for posting back or rendering.
+        /// </summary>
+        protected override void CreateChildControls( )
+        {
+            base.CreateChildControls();
+            Controls.Clear();
+
+            tbFrom = new RockTextBox();
+            tbFrom.ID = string.Format( "tbFrom_{0}", this.ID );
+            tbFrom.MaxLength = 11;
+            tbFrom.Label = "From";
+            tbFrom.Help = "The name the recipient will see as the message originating from.";
+            Controls.Add( tbFrom );
+
+            lFrom = new RockLiteral();
+            lFrom.ID = string.Format( "lFrom_{0}", this.ID );
+            lFrom.Label = "From";
+            Controls.Add( lFrom );
+
+            rcwMessage = new RockControlWrapper();
+            rcwMessage.ID = string.Format( "rcwMessage_{0}", this.ID );
+            rcwMessage.Label = "Message";
+            rcwMessage.Help = "<span class='tip tip-lava'></span>";
+            Controls.Add( rcwMessage );
+
+            mfpMessage = new MergeFieldPicker();
+            mfpMessage.ID = string.Format( "mfpMergeFields_{0}", this.ID );
+            mfpMessage.MergeFields.Clear();
+            mfpMessage.MergeFields.Add( "GlobalAttribute" );
+            mfpMessage.MergeFields.Add( "Rock.Model.Person" );
+            mfpMessage.CssClass += " pull-right margin-b-sm";
+            mfpMessage.SelectItem += mfpMergeFields_SelectItem;
+            rcwMessage.Controls.Add( mfpMessage );
+
+            tbMessage = new RockTextBox();
+            tbMessage.ID = string.Format( "tbTextMessage_{0}", this.ID );
+            tbMessage.TextMode = TextBoxMode.MultiLine;
+            tbMessage.Rows = 3;
+            rcwMessage.Controls.Add( tbMessage );
+
+            hfSenderName = new HiddenField();
+            hfSenderName.ID = string.Format( "hfSenderName_{0}", this.ID );
+            Controls.Add( hfSenderName );
+
+            hfSenderPhone = new HiddenField();
+            hfSenderPhone.ID = string.Format( "hfSenderPhone_{0}", this.ID );
+            Controls.Add( hfSenderPhone );
+        }
+
+        /// <summary>
+        /// Gets or sets the validation group.
+        /// </summary>
+        /// <value>
+        /// The validation group.
+        /// </value>
+        public override string ValidationGroup
+        {
+            get
+            {
+                EnsureChildControls();
+                return tbMessage.ValidationGroup;
+            }
+            set
+            {
+                EnsureChildControls();
+                tbFrom.ValidationGroup = value;
+                mfpMessage.ValidationGroup = value;
+                tbMessage.ValidationGroup = value;
+            }
+        }
+
+        /// <summary>
+        /// On new communicaiton, initializes controls from sender values
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <exception cref="System.NotImplementedException"></exception>
+        public override void InitializeFromSender( Person sender )
+        {
+            EnsureChildControls();
+            try
+            {
+                hfSenderPhone.Value = sender.PhoneNumbers
+                .Where( p => p.IsMessagingEnabled == true )
+                .FirstOrDefault()
+                .NumberFormattedWithCountryCode;
+            }
+            catch ( Exception )
+            {
+            }
+
+            hfSenderName.Value = string.Format( sender.NickName + " " + sender.LastName );
+
+            string organizationName = Rock.Web.Cache.GlobalAttributesCache.Read().GetValueFormatted( "OrganizationName" );
+            if ( organizationName.Length > 11 )
+            {
+                string organizationAbbreviation = Rock.Web.Cache.GlobalAttributesCache.Read().GetValueFormatted( "OrganizationAbbreviation" );
+                if ( !string.IsNullOrWhiteSpace( organizationAbbreviation ) & organizationAbbreviation.Length < 11 )
+                {
+                    organizationName = organizationAbbreviation;
+                }
+                else
+                {
+                    organizationName = organizationName.Replace( " ", string.Empty );
+                    organizationName = organizationName.Substring( 0, 11 );
+                }
+
+            }
+
+            if ( string.IsNullOrWhiteSpace( tbFrom.Text ) & !IsTemplate )
+            {
+                tbFrom.Text = organizationName;
+            }
+            if ( string.IsNullOrWhiteSpace( lFrom.Text ) )
+            {
+                lFrom.Text = organizationName;
+            }
+        }
+
+        /// <summary>
+        /// Outputs server control content to a provided <see cref="T:System.Web.UI.HtmlTextWriter" /> object and stores tracing information about the control if tracing is enabled.
+        /// </summary>
+        /// <param name="writer">The <see cref="T:System.Web.UI.HtmlTextWriter" /> object that receives the control content.</param>
+        public override void RenderControl( HtmlTextWriter writer )
+        {
+            tbFrom.Required = !IsTemplate;
+            tbMessage.Required = !IsTemplate;
+
+            if ( !UseSimpleMode )
+            {
+
+                tbFrom.RenderControl( writer );
+            }
+            else
+            {
+                lFrom.RenderControl( writer );
+            }
+            rcwMessage.RenderControl( writer );
+            hfSenderName.RenderControl( writer );
+            hfSenderPhone.RenderControl( writer );
+        }
+
+        #endregion
+
+        #region Events
+
+        void mfpMergeFields_SelectItem( object sender, EventArgs e )
+        {
+            EnsureChildControls();
+            tbMessage.Text += mfpMessage.SelectedMergeField;
+            mfpMessage.SetValue( string.Empty );
+        }
+
+        #endregion
+    }
+
+}
